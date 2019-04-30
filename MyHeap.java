67c27b849b7f7c5465f7dd6d265f16925cee9c15
James Chun <jchun00@stuy.edu>
2019-04-30 10:17:52 -0400

    huh:

--- MyHeap.java
+++ MyHeap.java
@@ -10,18 +10,19 @@ public class MyHeap{
 
     //We discussed these 2 methods already:
     private static void pushDown(int[]data,int size,int index){
-        while ( index < data.length ){ //if the child (left cause if theres no left theres no right) is out then stop
-            if ( data[index] > data[2(index)+1] ){
+        while ( 2 * index+1 < data.length && 2 * index+2 < data.length ){ //if the child (left cause if theres no left theres no right) is out then stop
+            if ( data[index] > data[2 * index+1] ){
                 int temp = data[index];
-                data[index] = data[2(index)+1];   //youre going through heap wrong, dont check all only index
-                data[2(index)+1] = temp;
+                data[index] = data[2 * index+1];   //youre going through heap wrong, dont check all only index
+                data[2 * index+1] = temp;
+                index = 2 * index+1;
             }
-            if ( data[index]>data[2(index)+2] ){
+            if ( data[index]>data[2 * index+2] ){
                 int temp = data[index];
-                data[index] = data[2(index)+2];
-                data[2(index)+2] = temp;
+                data[index] = data[2 * index+2];
+                data[2 * index+2] = temp;
+                index = 2 * index+2;
             }
-            index ++;
         }
     }
         /*
@@ -30,24 +31,34 @@ public class MyHeap{
          - precondition: index is between 0 and size-1 inclusive
          - precondition: size is between 0 and data.length-1 inclusive.*/
 
-    private static void pushUp(int[]data,int index){
 
+
+    private static void pushUp(int[]data,int index){
+      return;
     }
          /*- push the element at index i up into the correct position. This will swap it with the parent node until the parent node is larger or the root is reached. [ should be O(logn) ]
          - precondition: index is between 0 and data.length-1 inclusive.*/
 
 
     //We will discuss this today:
-    public static void heapify(int[]){
+    public static void heapify(int[] d){
+      return;
         //possibly could do it recursively
     }
         //- convert the array into a valid heap. [ should be O(n) ]
 
-    public static void heapsort(int[]){
-
+    public static void heapsort(int[] d){
+      return;
     }
         /*- sort the array [ should be O(nlogn) ] :
          converting it into a heap
          removing the largest value n-1 times (remove places at end of the sub-array).*/
+
+
+    public static void main(String[] args){
+      int[] test = {3,2,4};
+      pushDown(test,3,0);
+      System.out.println(test);
+    }
 
 }

a80a15cc0380628a2534b05d1adf5140eba76804
James-Chun <jchun00@stuy.edu>
2019-05-01 23:46:59 -0400

    have problem with heapify

--- MyHeap.java
+++ MyHeap.java
@@ -1,7 +1,7 @@
 public class MyHeap{
   private static void pushDown(int[]data,int size,int index){
       while ( size > 0 &&  2 * index+1 < data.length && 2 * index+2 < data.length ){ //if the child (left cause if theres no left theres no right) is out then stop
-
+          
           if ( data[2 * index+1] > data[2 * index+2] ){
               int temp = data[index];
               data[index] = data[2 * index+1];   //youre going through heap wrong, dont check all only index
@@ -28,11 +28,33 @@ public class MyHeap{
       }
   }
 
-    public static void main(String[] args){
-      int[] test = {3,2,4};
-      System.out.println(HeapHelp.toString(test));
-      pushDown(test,3,0);
-      System.out.println(HeapHelp.toString(test));
+
+  public static void heapify(int[] a){
+    int index = a.length - 1;
+    while (index >= 0){
+        if(2 * index + 2 > a.length - 1){
+            if (2 * index + 1 > a.length - 1){
+                index--;
+            }
+        }
+        // goes from bottom of the list to top to see if all the parents and children follow heap rules
+        pushDown(a, a.length, index);
+        // if not fix them
+        index --;
     }
+}
+
+public static void main(String[] args){
+  int[] test = {1,2,4,0,0,1,2};
+  System.out.println(HeapHelp.toString(test));
+  heapify(test);
+  //pushDown(test,3,0);
+  System.out.println(HeapHelp.toString(test));
+
+  //int[] test1 = {51,2,4,0,0,1,5};
+  //System.out.println(HeapHelp.toString(test1));
+  //pushUp(test1,6);
+//System.out.println(HeapHelp.toString(test1));
+}
 
 }
